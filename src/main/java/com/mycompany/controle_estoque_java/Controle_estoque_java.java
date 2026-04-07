0cc8fa0dc313406ba4c1e513ec30673562302aa5
Gui53 <gui53tube@gmail.com>
2026-04-07 20:01:00 -0300

    Init: Início do projeto

---
+++ src/main/java/com/mycompany/controle_estoque_java/Controle_estoque_java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.controle_estoque_java;
+
+/**
+ *
+ * @author guilh
+ */
+public class Controle_estoque_java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
