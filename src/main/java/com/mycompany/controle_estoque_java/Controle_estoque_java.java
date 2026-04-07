e5c1dbdcda004d4fb99c85a1846c01b0e0ee4395
Gui53 <gui53tube@gmail.com>
2026-04-07 20:07:05 -0300

    fix: remoção de comentários desnecessários

--- src/main/java/com/mycompany/controle_estoque_java/Controle_estoque_java.java
+++ src/main/java/com/mycompany/controle_estoque_java/Controle_estoque_java.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.controle_estoque_java;
 
-/**
- *
- * @author guilh
- */
 public class Controle_estoque_java {
 
     public static void main(String[] args) {
