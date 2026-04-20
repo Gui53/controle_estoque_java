55e0c673159be8fabedb6b96603023824beb243d
Gabriel Conci <gabrielconci20@gmail.com>
2026-04-19 21:30:03 -0300

    feat(model): implementa entidade Categoria - Cria classe Categoria conforme requisitos do sistema - Define atributos nome, tamanho e embalagem - Implementa construtores (vazio e parametrizado) - Adiciona métodos getters e setters

---
+++ src/main/java/dao/CategoriaDAO.java
@@ -0,0 +1,11 @@
+package dao;
+
+import model.Categoria;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoriaDAO {
+
+    private List<Categoria> lista = new ArrayList<>();
+
+}
---
+++ src/main/java/model/Categoria.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Categoria {
+
+    private String nome;
+    private String tamanho;
+    private String embalagem;
+
+    public Categoria() {
+    }
+
+    public Categoria(String nome, String tamanho, String embalagem) {
+        this.nome = nome;
+        this.tamanho = tamanho;
+        this.embalagem = embalagem;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getEmbalagem() {
+        return embalagem;
+    }
+
+    public void setEmbalagem(String embalagem) {
+        this.embalagem = embalagem;
+    }
+
+}
