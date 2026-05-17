4a12b0186f27b8f2d212565786e5a50f65eefe20
Gabriel Conci <gabrielconci20@gmail.com>
2026-05-17 14:24:54 -0300

    feat(dao): implementa CategoriaDAO com operações CRUD - Adiciona armazenamento de categorias em ArrayList - Implementa métodos de adicionar e listar categorias - Implementa busca por nome com equalsIgnoreCase - Adiciona atualização e remoção de categorias por nome

--- src/main/java/dao/CategoriaDAO.java
+++ src/main/java/dao/CategoriaDAO.java
@@ -1,11 +1,47 @@
 package dao;
 
-import model.Categoria;
 import java.util.ArrayList;
-import java.util.List;
+import model.Categoria;
 
 public class CategoriaDAO {
 
-    private List<Categoria> lista = new ArrayList<>();
+    private ArrayList<Categoria> lista = new ArrayList<>();
 
+    public void adicionar(Categoria categoria) {
+        lista.add(categoria);
+    }
+
+    public ArrayList<Categoria> listar() {
+        return lista;
+    }
+
+    public Categoria buscarPorNome(String nome) {
+        for (Categoria c : lista) {
+            if (c.getNome().equalsIgnoreCase(nome)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public boolean atualizar(String nome, Categoria novaCategoria) {
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).getNome().equalsIgnoreCase(nome)) {
+                lista.set(i, novaCategoria);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean remover(String nome) {
+        Categoria c = buscarPorNome(nome);
+
+        if (c != null) {
+            lista.remove(c);
+            return true;
+        }
+
+        return false;
+    }
 }
