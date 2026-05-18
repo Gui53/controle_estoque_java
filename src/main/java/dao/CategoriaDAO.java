bbf2a8f9aea21a78717212170664482b5b08da22
Gabriel Conci <gabrielconci20@gmail.com>
2026-05-18 20:15:56 -0300

    feat: inicio do insert de categoria para o banco de dados

--- src/main/java/dao/CategoriaDAO.java
+++ src/main/java/dao/CategoriaDAO.java
@@ -1,5 +1,11 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import model.Categoria;
 
@@ -44,4 +50,68 @@ public class CategoriaDAO {
 
         return false;
     }
+
+    public int maiorID() {
+        int maiorID = 0;
+        try {
+            Statement stmt = this.getConexao().createStatement();
+            ResultSet res = stmt.executeQuery("SELECT MAX(id) id FROM tb_alunos");
+            res.next();
+            maiorID = res.getInt("id");
+            stmt.close();
+        } catch (SQLException ex) {
+            System.out.println("Erro:" + ex);
+        }
+        return maiorID;
+    }
+
+   public boolean insertCategoria(Categoria objeto) {
+        String sql = "INSERT INTO tb_alunos(id,nome,idade,curso,fase) VALUES(?,?,?,?,?)";
+        try {
+            PreparedStatement stmt = this.getConexao().prepareStatement(sql);
+
+           /* stmt.setInt(1, objeto.getId());
+            stmt.setString(2, objeto.getNome());
+            stmt.setInt(3, objeto.getEmbalagem());
+            stmt.setString(4, objeto.getTamanho());
+ */
+
+            stmt.execute();
+            stmt.close();
+
+            return true;
+        } catch (SQLException erro) {
+            System.out.println("Erro:" + erro);
+            throw new RuntimeException(erro);
+        }
+    }
+
+    public Connection getConexao() {
+        Connection connection = null;
+        try {
+            String driver = "com.mysql.cj.jdbc.Driver";
+            Class.forName(driver);
+            String server = "localhost";
+            String database = "db_controledeestoque";
+            String url = "jdbc:mysql://" + server + ":3306/"
+                    + database + "?useTimezone=true&serverTimezone=UTC";
+            String user = "root";
+            String password = "SUA SENHA";
+            connection = DriverManager.getConnection(url, user, password);
+            if (connection != null) {
+                System.out.println("Status: Conectado!");
+            } else {
+                System.out.println("Status: NÃO CONECTADO!");
+            }
+            return connection;
+        } catch (ClassNotFoundException e) { //Driver não encontrado
+            System.out.println("O driver nao foi encontrado.");
+            return null;
+        } catch (SQLException e) {
+            System.out.println("Nao foi possivel conectar...");
+            return null;
+        }
+
+    }
+
 }
--- src/main/java/model/Categoria.java
+++ src/main/java/model/Categoria.java
@@ -1,23 +1,37 @@
 package model;
 
+import dao.CategoriaDAO;
 import enums.TipoEmbalagem;
 import enums.TipoTamanho;
 
 public class Categoria {
 
+    private int id;
     private String nome;
     private TipoTamanho tamanho;
     private TipoEmbalagem embalagem;
+    private CategoriaDAO dao;
 
     public Categoria() {
     }
 
-    public Categoria(String nome, TipoTamanho tamanho, TipoEmbalagem embalagem) {
+    public Categoria(int id, String nome, TipoTamanho tamanho, TipoEmbalagem embalagem) {
+        
+        this.id = id;
         this.nome = nome;
         this.tamanho = tamanho;
         this.embalagem = embalagem;
+        
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
     public String getNome() {
         return nome;
     }
@@ -42,6 +56,15 @@ public class Categoria {
         this.embalagem = embalagem;
     }
 
-    
+    public boolean insertCategoria(String nome, TipoTamanho tamanho, TipoEmbalagem embalagem) {
+        int id = this.maiorID() + 1;
+        Categoria objeto = new Categoria(id, nome, tamanho, embalagem);
+        dao.insertCategoria(objeto);
+        return true;
 
+    }
+
+    public int maiorID() {
+        return dao.maiorID();
+    }
 }
