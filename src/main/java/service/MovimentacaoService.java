39161faa1fd89d4847299114c8f5c0bde98b151d
Gui53 <gui53tube@gmail.com>
2026-05-21 18:55:31 -0300

    feat: criação do pacote service e classe MovimentacaoService, com a funcionalidade de registrar automaticamente a entrada de um produto

---
+++ src/main/java/service/MovimentacaoService.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package service;
+
+import dao.MovimentacaoDAO;
+import dao.ProdutoDAO;
+import enums.TipoMovimentacao;
+import java.time.LocalDate;
+import model.Movimentacao;
+import model.Produto;
+
+/**
+ *
+ * @author guilherme
+ */
+public class MovimentacaoService {
+
+    private ProdutoDAO produtoDAO = new ProdutoDAO();
+    private MovimentacaoDAO movimentacaoDAO = new MovimentacaoDAO();
+
+    public void entradaProduto(Produto produto, /*deve ser mudado para double*/ int quantidade) {
+
+        try {
+            produto.adicionar(quantidade);
+
+            //produtoDAO.update(produto);
+            Movimentacao movimentacao = new Movimentacao(produto, LocalDate.now(), quantidade, TipoMovimentacao.ENTRADA);
+
+            movimentacaoDAO.insert(movimentacao);
+
+            System.out.println("Entrada concluída!");
+        } catch (Exception e) {
+            System.out.println("Erro: " + e);
+        }
+    }
+
+}
