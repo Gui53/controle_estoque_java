27f44e870f0f55401208d7dd39bfc7c8ae74b90f
Gabriel Conci <gabrielconci20@gmail.com>
2026-05-24 01:02:45 -0300

    feat(principal): adiciona navegação para ProdutoView no botão Produtos do menu lateral

--- src/main/java/view/PrincipalView.java
+++ src/main/java/view/PrincipalView.java
@@ -34,6 +34,7 @@ public class PrincipalView extends javax.swing.JFrame {
 
         // Eventos dos botões
         btnCategorias.addActionListener(e -> new CategoriaView().setVisible(true));
+        btnProdutos.addActionListener(e -> new ProdutoView().setVisible(true));
         btnSair.addActionListener(e -> System.exit(0));
         setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
 
