bff22b560a675a4cedefe848ebf71c286a5ee246
enzolsilva <enzo.l.silva@gmail.com>
2026-05-24 17:45:09 -0300

    feat(relatorio): cria estrutura de relatorio para produtos abaixo, acima do maximo e valor total de estoque

--- src/main/java/dao/RelatorioDAO.java
+++ src/main/java/dao/RelatorioDAO.java
@@ -1,5 +1,23 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import util.ConnectionFactory;
+
 public class RelatorioDAO {
+    
+    public void produtosAbaixoMinimo(){
+    
+    }
+    
+    public void produtosAcimaMaximo(){
+        
+    }
+    
+    public void valorTotalEstoque() {
+        
+    } 
+    
     
 }
