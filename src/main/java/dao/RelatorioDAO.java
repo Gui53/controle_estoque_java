7b5d9cdef5b31a9181ccb64b0ec33060b50ba5c6
enzolsilva <enzo.l.silva@gmail.com>
2026-05-24 18:06:21 -0300

    feat(relatorio): adiciona relatorio de produtos abaixo do minimo

--- src/main/java/dao/RelatorioDAO.java
+++ src/main/java/dao/RelatorioDAO.java
@@ -9,6 +9,32 @@ public class RelatorioDAO {
     
     public void produtosAbaixoMinimo(){
     
+        String sql = """
+                SELECT nome, quantidade, minimo
+                FROM produto
+                WHERE quantidade < minimo
+            """;
+        
+        try (
+                Connection conn = ConnectionFactory.getConnection();
+                PreparedStatement stmt = conn.prepapeStatement(sql);
+                ResultSet rs = stmt.executeQuery()
+                ){
+            System.out.println("\n=== PRODUTOS ABAIXO DO MÍNIMO ===");
+            
+            while(rs.next()) {
+                System.out.println(
+                    rs.getString("nome")
+                    + " | Quantidade: "
+                    + rs.getInt("quantidade")
+                    + " | Mínimo: "
+                    + rs.getInt("minimo")
+                );                                   
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+            
     }
     
     public void produtosAcimaMaximo(){
