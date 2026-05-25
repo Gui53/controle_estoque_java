be987f05d3a64d03070cd07c10b87c2e83e2c30c
Gabriel Conci <gabrielconci20@gmail.com>
2026-05-25 18:57:34 -0300

    feat(main): altera ponto de entrada para abrir LoginView ao iniciar o sistema

--- src/main/java/main/Principal.java
+++ src/main/java/main/Principal.java
@@ -1,11 +1,10 @@
-
 package main;
 
-import view.PrincipalView;
+import view.LoginView;
 
 public class Principal {
 
     public static void main(String[] args) {
-        java.awt.EventQueue.invokeLater(() -> new PrincipalView().setVisible(true));
+        java.awt.EventQueue.invokeLater(() -> new LoginView().setVisible(true));
     }
 }
