36edf414ee541cca9790a0e5c5494e22336c14f4
Gui53 <gui53tube@gmail.com>
2026-05-26 22:15:09 -0300

    docs: adiciona documentação na classe movimentacaoService

--- src/main/java/service/MovimentacaoService.java
+++ src/main/java/service/MovimentacaoService.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package service;
 
 import dao.MovimentacaoDAO;
@@ -12,32 +8,58 @@ import model.Movimentacao;
 import model.Produto;
 
 /**
- *
- * @author guilherme
+ * Classe responsável pelas regras de negócio
+ * relacionadas às movimentações de estoque.
+ * 
+ * @author Guilherme
  */
 public class MovimentacaoService {
 
     private ProdutoDAO produtoDAO = new ProdutoDAO();
     private MovimentacaoDAO movimentacaoDAO = new MovimentacaoDAO();
 
+    /**
+     * Realiza a entrada de produtos no estoque.
+     * 
+     * @param produto Produto movimentado
+     * @param quantidade Quantidade adicionada ao estoque
+     */
     public void entradaProduto(Produto produto, double quantidade) {
 
         try {
+
             produto.adicionar(quantidade);
 
             produtoDAO.updateQuantidade(produto);
-            Movimentacao movimentacao = new Movimentacao(produto, LocalDate.now(), quantidade, TipoMovimentacao.ENTRADA);
+
+            Movimentacao movimentacao =
+                    new Movimentacao(
+                            produto,
+                            LocalDate.now(),
+                            quantidade,
+                            TipoMovimentacao.ENTRADA
+                    );
 
             movimentacaoDAO.insert(movimentacao);
 
             System.out.println("Entrada concluída!");
+
         } catch (RuntimeException e) {
+
             System.out.println("Erro: " + e);
         }
     }
 
+    /**
+     * Realiza a saída de produtos do estoque.
+     * 
+     * @param produto Produto movimentado
+     * @param quantidade Quantidade removida do estoque
+     */
     public void saidaProduto(Produto produto, double quantidade) {
+
         try {
+
             boolean removeu = produto.remover(quantidade);
 
             if (!removeu) {
@@ -46,15 +68,22 @@ public class MovimentacaoService {
             }
 
             produtoDAO.updateQuantidade(produto);
-            Movimentacao movimentacao = new Movimentacao( produto, LocalDate.now(), quantidade, TipoMovimentacao.SAIDA);
+
+            Movimentacao movimentacao =
+                    new Movimentacao(
+                            produto,
+                            LocalDate.now(),
+                            quantidade,
+                            TipoMovimentacao.SAIDA
+                    );
 
             movimentacaoDAO.insert(movimentacao);
 
             System.out.println("Saida Realizada!");
 
         } catch (RuntimeException e) {
-            System.out.println("Erro: " + e);
 
+            System.out.println("Erro: " + e);
         }
     }
 }
