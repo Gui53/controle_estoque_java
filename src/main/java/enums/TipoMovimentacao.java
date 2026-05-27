0a655c73c85190976c98c86eb7d4b525416db422
Gui53 <gui53tube@gmail.com>
2026-05-26 22:17:55 -0300

    docs: documentação do enum TipoMovimentacao

--- src/main/java/enums/TipoMovimentacao.java
+++ src/main/java/enums/TipoMovimentacao.java
@@ -1,5 +1,14 @@
 package enums;
 
+/**
+ * Enum responsável pelos tipos de movimentação
+ * realizadas no estoque.
+ * 
+ * ENTRADA representa adição de produtos.
+ * SAIDA representa retirada de produtos.
+ * 
+ * @author Guilherme
+ */
 public enum TipoMovimentacao {
     ENTRADA,
     SAIDA
