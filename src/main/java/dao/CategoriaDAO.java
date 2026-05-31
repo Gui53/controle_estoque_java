3c9c93509daa204d0298a1a5292363ba3286a95b
Gabriel Conci <gabrielconci20@gmail.com>
2026-05-31 14:10:58 -0300

    docs: adiciona comentários Javadoc no CategoriaDAO
    - Adiciona @author na classe
    - Documenta todos os métodos com Javadoc
    - Descreve parâmetros com @param e retornos com @return
    - Adiciona @throws no método montarCategoria

--- src/main/java/dao/CategoriaDAO.java
+++ src/main/java/dao/CategoriaDAO.java
@@ -10,8 +10,21 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import modelo.Categoria;
 
+/**
+ * Classe responsável pelas operações de banco de dados relacionadas às
+ * categorias.
+ *
+ * @author Gabriel Conci
+ * @see java.sql.Connection
+ */
 public class CategoriaDAO {
 
+    /**
+     * Insere uma categoria no banco de dados.
+     *
+     * @param objeto Categoria a ser cadastrada
+     * @return boolean Retorna true caso a inserção seja realizada
+     */
     public boolean inserir(Categoria objeto) {
 
         String sql = "INSERT INTO tb_categoria(nome,tamanho,embalagem) VALUES(?,?,?)";
@@ -39,6 +52,11 @@ public class CategoriaDAO {
         }
     }
 
+    /**
+     * Recupera todas as categorias cadastradas no banco de dados.
+     *
+     * @return ArrayList Lista de categorias
+     */
     public ArrayList<Categoria> visualizar() {
 
         ArrayList<Categoria> lista = new ArrayList<>();
@@ -69,6 +87,12 @@ public class CategoriaDAO {
         return lista;
     }
 
+    /**
+     * Recupera uma categoria pelo seu identificador.
+     *
+     * @param id Identificador da categoria
+     * @return Categoria Categoria encontrada ou null caso não exista
+     */
     public Categoria selecionarPorId(int id) {
 
         String sql = "SELECT * FROM tb_categoria WHERE id = ?";
@@ -104,6 +128,12 @@ public class CategoriaDAO {
         return null;
     }
 
+    /**
+     * Atualiza os dados de uma categoria no banco de dados.
+     *
+     * @param objeto Categoria com os dados atualizados
+     * @return boolean Retorna true caso a atualização seja realizada
+     */
     public boolean atualizar(Categoria objeto) {
 
         String sql = "UPDATE tb_categoria SET nome = ?, tamanho = ?, embalagem = ? WHERE id = ?";
@@ -130,6 +160,12 @@ public class CategoriaDAO {
         }
     }
 
+    /**
+     * Remove uma categoria do banco de dados pelo seu identificador.
+     *
+     * @param id Identificador da categoria a ser removida
+     * @return boolean Retorna true caso a remoção seja realizada
+     */
     public boolean apagar(int id) {
 
         String sql = "DELETE FROM tb_categoria WHERE id = ?";
@@ -153,17 +189,24 @@ public class CategoriaDAO {
         return true;
     }
 
+    /**
+     * Monta um objeto Categoria a partir do resultado de uma query.
+     *
+     * @param res ResultSet com os dados da categoria
+     * @return Categoria Objeto montado com os dados do banco
+     * @throws SQLException Caso ocorra erro na leitura do ResultSet
+     */
     private Categoria montarCategoria(ResultSet res) throws SQLException {
 
         int id = res.getInt("id");
 
         String nome = res.getString("nome");
 
-        TipoEmbalagem tipoEmbalagem =
-                TipoEmbalagem.valueOf(res.getString("embalagem"));
+        TipoEmbalagem tipoEmbalagem
+                = TipoEmbalagem.valueOf(res.getString("embalagem"));
 
-        TipoTamanho tipoTamanho =
-                TipoTamanho.valueOf(res.getString("tamanho"));
+        TipoTamanho tipoTamanho
+                = TipoTamanho.valueOf(res.getString("tamanho"));
 
         return new Categoria(
                 id,
