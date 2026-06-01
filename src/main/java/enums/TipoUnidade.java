f7ea8304d782b32b0dc524a35427a70c662dcc42
Gabriel Conci <gabrielconci20@gmail.com>
2026-06-01 20:38:11 -0300

    docs: adiciona comentários JavaDoc no enum TipoUnidade

--- src/main/java/enums/TipoUnidade.java
+++ src/main/java/enums/TipoUnidade.java
@@ -1,9 +1,37 @@
 package enums;
 
+/**
+ * Enumeração que representa as unidades de medida
+ * utilizadas pelos produtos cadastrados no sistema.
+ *
+ * Os valores definidos permitem controlar o estoque
+ * de acordo com a forma de comercialização ou armazenamento
+ * de cada produto.
+ *
+ * @author Gabriel Conci
+ * @version 1.0
+ * @since 2026
+ */
 public enum TipoUnidade {
+
+    /**
+     * Unidade de medida em quilogramas.
+     */
     KG,
+
+    /**
+     * Unidade de medida em litros.
+     */
     LITRO,
+
+    /**
+     * Unidade de medida por unidade individual.
+     */
     UNIDADE,
+
+    /**
+     * Unidade de medida por pacote.
+     */
     PACOTE
-    
+
 }
