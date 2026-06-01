5b6b0d788b4fd894fcb08000692d54553fc8fbc2
Gabriel Conci <gabrielconci20@gmail.com>
2026-06-01 20:37:29 -0300

    docs: adiciona comentários JavaDoc no enum TipoTamanho

--- src/main/java/enums/TipoTamanho.java
+++ src/main/java/enums/TipoTamanho.java
@@ -1,7 +1,30 @@
 package enums;
 
+/**
+ * Enumeração que representa os tamanhos disponíveis
+ * para as categorias de produtos cadastradas no sistema.
+ *
+ * Os valores definidos permitem classificar os produtos
+ * conforme seu porte ou volume.
+ *
+ * @author Gabriel Conci
+ * @version 1.0
+ * @since 2026
+ */
 public enum TipoTamanho {
+
+    /**
+     * Representa um produto de pequeno porte.
+     */
     PEQUENO,
+
+    /**
+     * Representa um produto de porte médio.
+     */
     MEDIO,
+
+    /**
+     * Representa um produto de grande porte.
+     */
     GRANDE
 }
