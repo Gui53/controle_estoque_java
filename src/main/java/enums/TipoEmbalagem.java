1040c540eaabfcebda86c672b6b20dea7509f768
Gabriel Conci <gabrielconci20@gmail.com>
2026-06-01 20:36:43 -0300

    docs: adiciona comentários JavaDoc no enum TipoEmbalagem

--- src/main/java/enums/TipoEmbalagem.java
+++ src/main/java/enums/TipoEmbalagem.java
@@ -1,7 +1,30 @@
 package enums;
 
+/**
+ * Enumeração que representa os tipos de embalagem
+ * disponíveis para as categorias de produtos.
+ *
+ * Os valores definidos são utilizados para classificar
+ * a forma de armazenamento ou comercialização dos produtos.
+ *
+ * @author Gabriel Conci
+ * @version 1.0
+ * @since 2026
+ */
 public enum TipoEmbalagem {
+
+    /**
+     * Produto armazenado em embalagem do tipo lata.
+     */
     LATA,
+
+    /**
+     * Produto armazenado em embalagem de vidro.
+     */
     VIDRO,
+
+    /**
+     * Produto armazenado em embalagem plástica.
+     */
     PLASTICO
 }
